9761c70a7e598f8a698870d066c6ca344a0e463d
gel1ler <abanichev16@gmail.com>
2024-12-27 22:52:38 +0300

    fix

--- app/api/permissions/services/permissions.ts
+++ app/api/permissions/services/permissions.ts
@@ -2,9 +2,24 @@ import { Page } from "puppeteer"
 
 export async function grantPermissions(page: Page, emails: string) {
     try {
-        //screenshot
         await page.click('.iv-ui-icon-button.iv-ui-icon-button__m-size-medium.iv-ui-icon-button__m-theme-light-stroke')
+        await page.screenshot({ path: './public/1.png' })
         await page.click('.iv-ui-dropdown-menu__content :nth-child(5)')
+        await page.screenshot({ path: './public/2.png' })
+        const element = await page.evaluate(() => {
+            const elements = Array.from(document.querySelectorAll('.iv-ui-btn-text'));
+            const targetElement = elements.find(element => element.textContent && element.textContent.includes('Передать'));
+            if (targetElement && targetElement instanceof HTMLElement) {
+                targetElement.click();
+                return true;
+            }
+            return false;
+        });
+
+        if (!element) {
+            throw new Error('Элемент с текстом "Передать" не найден');
+        }
+
         const checkboxes = await page.$$('input[type="checkbox"]')
         await checkboxes[1].click()
         await page.click('.iv-ui-btn.iv-ui-btn__m-theme-light-primary-filled.iv-ui-btn__m-size-medium')
@@ -26,8 +41,27 @@ export async function denyPermissions(page: Page, emails: string) {
             throw new Error("Page is not available");
         }
         await page.click('.iv-ui-icon-button.iv-ui-icon-button__m-size-medium.iv-ui-icon-button__m-theme-light-stroke');
-        await page.click('.iv-ui-dropdown-menu__content :nth-child(6)');
-        await page.waitForSelector('.iv-my-rights-list-item__control', { timeout: 10000 });
+
+        // Поиск и клик по элементу с текстом "Редактировать"
+        const editButtonClicked = await page.evaluate(() => {
+            const dropdownContent = document.querySelector('.iv-ui-dropdown-menu__content');
+            if (!dropdownContent) {
+                return false
+            }
+            const editButton = Array.from(dropdownContent.children).find(
+                (element) => element.textContent?.includes('Редактировать')
+            );
+            if (editButton && editButton instanceof HTMLElement) {
+                editButton.click()
+                return true
+            }
+            return false
+        });
+        if (!editButtonClicked) {
+            throw new Error('Элемент с текстом "Редактировать" не найден');
+        }
+
+        await page.waitForSelector('.iv-my-rights-list-item__control', { timeout: 3000 });
         const items = await page.$$('.iv-my-rights-list-grantee-access-item');
 
         for (const item of items) {
@@ -66,12 +100,29 @@ export async function denyAllPermissions(page: Page) {
             throw new Error("Page is not available");
         }
         await page.click('.iv-ui-icon-button.iv-ui-icon-button__m-size-medium.iv-ui-icon-button__m-theme-light-stroke');
-        await page.click('.iv-ui-dropdown-menu__content :nth-child(6)');
-        await page.waitForSelector('.iv-my-rights-list-item__control', { timeout: 10000 });
+        // Поиск и клик по элементу с текстом "Редактировать"
+        const editButtonClicked = await page.evaluate(() => {
+            const dropdownContent = document.querySelector('.iv-ui-dropdown-menu__content');
+            if (!dropdownContent) {
+                return false
+            }
+            const editButton = Array.from(dropdownContent.children).find(
+                (element) => element.textContent?.includes('Редактировать')
+            );
+            if (editButton && editButton instanceof HTMLElement) {
+                editButton.click()
+                return true
+            }
+            return false
+        });
+        if (!editButtonClicked) {
+            throw new Error('Элемент с текстом "Редактировать" не найден');
+        }
+        await page.waitForSelector('.iv-my-rights-list-item__control', { timeout: 3000 });
         let items = await page.$$('.iv-my-rights-list-grantee-access-item');
-
+        
         let index = 0;
-        while (items.length > 1) {
+        while (items.length >= 1) {
             const itemText = await items[index].evaluate(node => node.textContent);
             if (itemText?.includes('botneva02@gmail.com')) index++;
             else {
